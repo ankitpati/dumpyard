d37f0a7c748b9f728fef114e9f3130a5e3214a04
Ankit Pati <contact@ankitpati.in>
2018-01-06 22:31:14 +0530

    interpreter to add all numbers in a script

---
+++ src/adding.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void parse_error(char *s)
+{
+    fprintf(stderr, "%s", s);
+    exit(2);
+}
+
+int main(int argc, char **argv)
+{
+#define NUMSTR(x) \
+    do { \
+        numstr = realloc(numstr, num_i + 1); \
+        numstr[num_i++] = (x); \
+    } while (0)
+
+    int c;
+    char *numstr;
+    size_t num_i, i;
+    double acc;
+    FILE *fin;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage:\n\tadding <scriptname>\n");
+        exit(1);
+    }
+
+    if (!(fin = fopen(argv[1], "r"))) {
+        fprintf(stderr, "Script could not be opened!\n");
+        exit(5);
+    }
+
+    acc = 0.0;
+    numstr = NULL;
+    num_i = 0;
+
+    c = fgetc(fin);
+    while (!feof(fin) && !ferror(fin)) {
+        switch (c) {
+        case '\n':
+            if (numstr) {
+                NUMSTR('\0');
+                acc += atof(numstr);
+                free(numstr);
+                numstr = NULL;
+                num_i = 0;
+            }
+            break;
+
+        case '-':
+        case '+':
+            if (!numstr)
+                NUMSTR(c);
+            else
+                parse_error("+/- can only appear at beginning of number.\n");
+            break;
+
+        case '.':
+            for (i = 0; i < num_i && numstr[i] != '.'; ++i);
+            if (i == num_i)
+                NUMSTR(c);
+            else
+                parse_error("Only one decimal point allowed in a number.\n");
+            break;
+
+        case '#':
+            while ((c = fgetc(fin)) != '\n')
+                if (feof(fin) || ferror(fin))
+                    break;
+            /* intended fall-through */
+
+        default:
+            if (isspace(c)) {
+                if (numstr) {   /* OPTIONAL! For performance only. */
+                    NUMSTR('\0');
+                    acc += atof(numstr);
+                    free(numstr);
+                    numstr = NULL;
+                    num_i = 0;
+                }
+            }
+
+            else if (isdigit(c))
+                NUMSTR(c);
+
+            else
+                parse_error("Only sign, numbers, and decimal point allowed.\n");
+
+            break;
+        }
+
+        c = fgetc(fin);
+    }
+
+    printf("Sum = %lf\n", acc);
+    return 0;
+
+#undef NUMSTR
+}
