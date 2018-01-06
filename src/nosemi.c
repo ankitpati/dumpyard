b5acdca37f43560ead0463e2bf96599b95094945
Ankit Pati <contact@ankitpati.in>
2018-01-06 22:20:03 +0530

    no semicolon

---
+++ src/nosemi.c
@@ -0,0 +1,11 @@
+/* nosemi.c */
+/* This program does not use a semicolon, yet does something meaningful. */
+
+#include <stdio.h>
+
+int main()
+{
+    if (printf("hello, world\n")) {
+    }
+}
+/* end of nosemi.c */
